811bdffb26370410dcb73cd686009ca6571b3827
Sergio <69847852+SergioM0@users.noreply.github.com>
2023-02-15 23:55:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 function myFunction() {
     var elemento = document.getElementById('pls').value;
-      if (elemento == 1) {
-          var div = document.createElement('div');
-          document.body.appendChild(div);
-          var content = document.createTextNode("whoami ejeje");
-          div.appendChild(content);
-
-          switch (pls) {
-              case 1:
-              console.log("1");
-                  break;
-              case 2:
-              console.log("");
-                  break;
-          }
+    if (elemento == 1) {
     }
-}
+
+ 
+
+    switch (elemento) {
+        case "1":
+            var div = document.createElement('div')
+            div.setAttribute('id', 'output');
+            document.body.appendChild(div);
+            var content = document.createTextNode(whoami);
+            div.appendChild(content);
+            break;
+        case "2":
+            var div = document.createElement('div');
+            document.body.appendChild(div);
+            var content = document.createTextNode("I dont really do nothing");
+            div.appendChild(content);
+            break;
+        case "3":
+            output.remove();
+            break;
+
+        default:
+            alert("ERROR");
+    } 
+    }
+    
+
